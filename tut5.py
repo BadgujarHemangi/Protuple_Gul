b1388830fb5b78e072d9261940febb04b86c4a02
hema <hemangibadgujar83@gmail.com>
2022-02-19 11:07:10 +0530

    add button

---
+++ tut5.py
@@ -0,0 +1,21 @@
+from tkinter import*
+
+def hello():
+    print("hello tkinter button")
+def name():
+    print("name is hema")
+root=Tk()
+root.geometry("675x435")
+frame=Frame(root,borderwidth=6,bg="pink",relief=RAISED)
+frame.pack(side=LEFT,anchor=NW)
+
+b1= Button(frame,bg="red",text="Print now",command=hello)
+b1.pack(side=RIGHT,padx=23,pady=24)
+
+b2= Button(frame,bg="red",text="tell now",command=name)
+b2.pack(side=TOP,padx=23,pady=24)
+
+b3= Button(frame,bg="red",text="Print now")
+b3.pack(side=BOTTOM,padx=23,pady=24)
+
+root.mainloop()
