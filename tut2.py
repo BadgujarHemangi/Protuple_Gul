23f8ad4a07456c0a24c592ca01da272c9203a1ad
hema <hemangibadgujar83@gmail.com>
2022-02-19 09:22:23 +0530

    add imange

---
+++ tut2.py
@@ -0,0 +1,9 @@
+from tkinter import*
+
+hemangi_root = Tk()
+hemangi_root.geometry("455x244")
+photo = PhotoImage(file ="C:\\Users\\Hema's PC\\Pictures\\fancy-line-png-11552252746xsn7aqxrgj.png ")
+a_Label=Label(image = photo)
+a_Label.pack()
+
+hemangi_root.mainloop()
